8e7cca4f7fbd8f4db2b7e06f86cd10d59055e547
chrkaw152 <114194285+chrkaw152@users.noreply.github.com>
2023-10-03 16:24:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+//require express
+const express = require("express");
+//instantiate express
+const app = express();
 
+//When the client sends a get request to the / endpoint, send the /src/pages/index.html file
+
+//make sure the server is listening on port 3000
+app.listen(process.env.PORT || 3000, function() {
+  console.log(`Server is running on port ${process.env.PORT}.`);
+});
