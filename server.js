a62351696adde4452297e884d4c31537ff6c5f62
chrkaw152 <114194285+chrkaw152@users.noreply.github.com>
2023-10-03 16:43:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const express = require("express");
 //instantiate express
 const app = express();
 
+//serve files in the public folder
+app.use(express.static("./public"));
+
 //When the client sends a get request to the / endpoint, send the /src/pages/index.html file
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/src/pages/index.html");
+}
+
+//When the client sends a post request to the / endpoint, send the /src/pages/submitted.html file
+app.post("/", function(req, res) {
+  alert("wow it worked");
+}
 
 //make sure the server is listening on port 3000
 app.listen(process.env.PORT || 3000, function() {
